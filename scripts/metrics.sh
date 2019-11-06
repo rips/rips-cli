26fcc3cdcfb88a9e0f0724cf0c06fdb11ebd37f7
Hendrik Buchwald <hbuchwald@ripstech.com>
2019-11-06 16:32:03 +0100

    Also load all commands for tests

--- scripts/metrics.sh
+++ scripts/metrics.sh
@@ -5,6 +5,8 @@ SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "${SCRIPT_PATH}/.."
 
 export SYMFONY_ENV=test
+export APP_ENV=test
+export RIPS_CLI_ALL=1
 composer install --dev --no-interaction
 
 rm -rf phpmetrics
