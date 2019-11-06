2c051097508b72855aea390376e36e26d5d48256
Hendrik Buchwald <hbuchwald@ripstech.com>
2019-11-06 15:36:12 +0100

    Fix hadolint warnings

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -5,8 +5,8 @@ SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "${SCRIPT_PATH}/.."
 
 hadolint --ignore DL3008 --ignore DL3015 docker/Dockerfile
-hadolint --ignore DL3008 --ignore DL3015 --ignore DL3016 --ignore DL4001 scripts/build_env/Dockerfile
-hadolint --ignore DL3008 --ignore DL3015 --ignore DL3016 --ignore DL4001 scripts/test_env/Dockerfile
+hadolint --ignore DL3008 --ignore DL3015 scripts/build_env/Dockerfile
+hadolint --ignore DL3008 --ignore DL3015 scripts/test_env/Dockerfile
 
 export SYMFONY_ENV=test
 composer install --dev --no-interaction
